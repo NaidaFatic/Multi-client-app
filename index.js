6118ea34670286d5ebe46a2c2b585efbca74db83
NaidaFatic <faticnaida@gmail.com>
2021-05-31 00:31:23 +0200

    first node.js

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+//index.js
+//Import Express
 let express = require('express')
-
+//Start App
 let app = express();
-
+//Assign port
 var port = process.env.PORT || 8080;
 // Welcome message
 app.get('/', (req, res) => res.send('Welcome to Express'));
