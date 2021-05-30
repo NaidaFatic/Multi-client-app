aa024836a5288322922592af2ba0e3d800ff8069
NaidaFatic <faticnaida@gmail.com>
2021-05-31 01:47:05 +0200

    first route

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 //index.js
-//Import Express
+//Import
 let express = require('express')
+let apiRoutes = require("./api/routes.js")
 //Start App
 let app = express();
 //Assign port
 var port = process.env.PORT || 8080;
 // Welcome message
 app.get('/', (req, res) => res.send('Welcome to Express'));
+//Use API routes in the App
+app.use('/api', apiRoutes)
 // Launch app to the specified port
 app.listen(port, function() {
     console.log("Running FirstRest on Port "+ port);
