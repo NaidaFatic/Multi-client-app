5d2d2da0fc112c251e0cb200e936f90e5e681b04
NaidaFatic <faticnaida@gmail.com>
2021-06-26 23:22:26 +0200

    test4

--- index.js
+++ index.js
@@ -1,32 +1,18 @@
-require('dotenv').config()
 let express = require('express')
 let app = express();
-
-const user = process.env.DB_USER;
-const pass = process.env.DB_PASS;
-const collection = process.env.DB_COLLECTION;
-
 //import body parser
 let bodyParser = require('body-parser');
 var port = process.env.PORT || 8080;
-var cors = require('cors')
-
-app.use(cors())
-
-var corsOptions = {
-  origin: 'https://multi-client-front.vercel.app/',
-  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
-
-app.get('/', cors(corsOptions), function (req, res, next) {
-  res.json({msg: 'This is backend!'}) //CORS-enabled for all origins!
-})
-
 //configure bodyparser to hande the post requests
 app.use(bodyParser.urlencoded({
     extended: true
 }));
 app.use(bodyParser.json());
+// Welcome message
+app.post("/", (req, res) => {
+  console.log("Connected to React");
+  res.redirect("http://localhost:3000");
+});
 // Launch app to the specified port
 app.listen(port, function() {
     console.log("Running on Port "+ port);
@@ -39,9 +25,8 @@ app.use('/api/users', usersRoutes)
 app.use('/api/articles', articlesRoutes)
 //import mongoose
 let mongoose = require('mongoose');
-
 //connect to mongoose
-const dbPath = 'mongodb+srv://'+user+':'+pass+'@'+collection+'.4wxap.mongodb.net/multi-client?retryWrites=true&w=majority';
+const dbPath = 'mongodb+srv://naida:naida@test.4wxap.mongodb.net/multi-client?retryWrites=true&w=majority';
 const options = {useNewUrlParser: true, useUnifiedTopology: true}
 const mongo = mongoose.connect(dbPath, options);
 mongo.then(() => {
